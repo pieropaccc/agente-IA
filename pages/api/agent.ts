de3fa97b2c0619f8523949cbeaa4487212b9e07e
pieropacella <pieropaccc@gmail.com>
2025-09-17 14:46:34 -0300

    agregar eleminar js de agent.ts

--- pages/api/agent.ts
+++ pages/api/agent.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { runAgent } from "../../lib/agent.js";
+import { runAgent } from "../../lib/agent";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
